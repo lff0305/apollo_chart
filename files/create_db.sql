cf5700ed7dd2cc0b014a789e393de9d5ac68197c
lff0305 <lff0305@gmail.com>
2021-07-16 20:31:00 +0800

    No issue. Update code

---
+++ files/create_db.sql
@@ -0,0 +1,12 @@
+{{- range .Values.envs }}
+{{- with . }}
+{{ $configMapFullName := printf "apollo-configmap-%s" . -}}
+{{ $configDBSuffix := printf "%s" . -}}
+{{ $configServiceFullName := printf "apollo-config-service-%s" . -}}
+{{ $adminServiceFullName := printf "apollo-admin-service-%s" . -}}
+
+create database IF NOT EXISTS] {{ $.Values.configdb.dbName }}-{{ $configDBSuffix}} DEFAULT CHARACTER SET = utf8mb4;
+grant all on {{ $.Values.configdb.dbName }}-{{ $configDBSuffix}}.* to  {{ $.Values.configdb.userName }}@'%' IDENTIFIED by '{{ $.Values.configdb.password }}'
+
+{{- end }}
+{{ end }}
